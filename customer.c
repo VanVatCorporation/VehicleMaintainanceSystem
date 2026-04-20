b07985de3676dcc156320a8432025251e0ef3882
thach09 <thietthachdo@gmail.com>
2026-04-20 23:39:43 +0700

    Text Commit

--- customer.c
+++ customer.c
@@ -1,3 +1,8 @@
 // Thêm, sửa, tìm kiếm khách hàng theo SĐT hoặc biển số
 
 #include <stdio.h>
+
+addCustomer() {
+    // Code to add a customer
+    printf("Adding a customer...\n");
+}
--- customer.h
+++ customer.h
@@ -1,1 +1,6 @@
 // Thêm, sửa, tìm kiếm khách hàng theo SĐT hoặc biển số
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#endif // CUSTOMER_H    
+void addCustomer();
