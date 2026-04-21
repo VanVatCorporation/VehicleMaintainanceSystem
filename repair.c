65e5820650d9b2e807556e39976f85e4f197139a
Hoàng Minh Trọng <131216020+Hoang-Trong-1@users.noreply.github.com>
2026-04-21 14:15:04 +0700

    21/3

--- repair.c
+++ repair.c
@@ -2,3 +2,35 @@
 // tiền
 
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "repair.h"
+#include "customer.h"
+
+void generateOrderId(char orderId[], int counter){
+	sprintf(orderId, "RO%06d", counter);
+}
+
+int main(){
+	RepairOrder order;
+	
+	generateOrderId(order.orderId, 1); // mỗi lần tạo phiếu sẽ tăng lên 1
+
+    printf("Nhap so dien thoai: ");
+    fgets(inputPhone, sizeof(inputPhone), stdin);
+    inputPhone[strcspn(inputPhone, "\n")] = 0;
+	
+	printf("Nhap tinh trang xe:");
+	fgets(order.symptom, sizeof(order.symptom), stdin);
+	order.symptom[strcspn(order.symptom, "\n")] = 0;
+	
+	order.createdDate=time(NULL); // thời gian thực khi tạo phiếu 
+	
+	printf("\n=== THONG TIN PHIEU ===\n");
+	printf("Order ID: %s\n", order.orderId);
+    printf("Phone: %s\n", )
+	printf("Symptom: %s\n", order.symptom);
+	printf("Created: %s", ctime(&order.createdDate));
+	
+	return 0;
+}
--- repair.h
+++ repair.h
@@ -1,2 +1,24 @@
-// Tạo phiếu, thêm dịch vụ vào phiếu, cập nhật trạng thái, tính tổng
-// tiền
+// Tạo phiếu, thêm dịch vụ vào phiếu, cập nhật trạng thái, tính tổng tiền
+
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <time.h>
+
+typedef enum {
+    TIEP_NHAN,
+    DANG_SUA,
+    HOAN_THANH
+} Status;
+
+typedef struct RepairOrder {
+    char orderId[10];
+    char customerPhone[15];
+    char symptom[100];3
+    time_t createdDate;
+    Status status;
+}RepairOrder;
+
+void createOrder(struct RepairOrder orders[], int *m, struct Customer customers[], int n);
+
+#endif
